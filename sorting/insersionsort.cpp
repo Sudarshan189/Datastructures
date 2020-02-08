cdca602d08f90c27d5c17e9e438d55a6bfb69de9
Sudarshan Shanbhag <shanbhagsudharshan@gmail.com>
2020-02-08 16:38:07 +0530

    Create insersionsort.cpp

---
+++ sorting/insersionsort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> compareTheOld(int n, vector<int> arr, int sortedPos) {
+    
+}
+
+vector<int> insertionSort(int n, vector<int> arr) {
+   int sortedPos=0;
+   int temp1;
+   int tempPos;
+   for(int i=1;i<n;i++) {
+       tempPos=i;
+       while(tempPos != 0) {
+        if(arr[tempPos] < arr[tempPos-1]) {
+            temp1 = arr[tempPos];
+            arr[tempPos] = arr[tempPos-1];
+            arr[tempPos-1] = temp1;
+            tempPos--;
+            continue;
+        }
+        break;
+       }
+   }
+    return arr;
+}
+
+int main() {
+ vector<int> arr={8,9,2,4,7,3,5,89,70,-7,8};
+    
+  arr = insertionSort(arr.size(),arr);
+  cout << "\n";
+  for(int a: arr) {
+        cout << a << " ";
+    }
+   return 0;
+}
